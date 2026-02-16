9ac6fb084549a550cf8057e0a6de7d792e026497
Александр <13aalleexx76@gmail.com>
2026-02-16 20:02:24 +1000

    template_pack_v1: add plugin entrypoints (referral-connector, age-gate)

---
+++ template_pack_v1/wp-content/plugins/age-gate/age-gate.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Age Gate
+ * Description: Sitewide 18+ gate with remember=forever and on/off toggle.
+ * Version: 0.1.0
+ * Author: Template Pack v1
+ */
+
+if (!defined("ABSPATH")) { exit; }
+
+// TODO(MVP): implement sitewide AgeGate + remember forever + per-site toggle.
---
+++ template_pack_v1/wp-content/plugins/referral-connector/referral-connector.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Referral Connector
+ * Description: Connects WooCommerce store to Referral Service (click/order-paid/refund events).
+ * Version: 0.1.0
+ * Author: Template Pack v1
+ */
+
+if (!defined("ABSPATH")) { exit; }
+
+// TODO(MVP): implement connector per spec M/L.
